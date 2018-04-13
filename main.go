080f959441b26b3bddf1227d4392d0ff95e87302
rinosukmandityo <rino.sukmandityo@gmail.com>
2018-04-13 14:00:40 +0530

    adding counter and also read & save via mongodb

--- main.go
+++ main.go
@@ -2,17 +2,58 @@ package main
 
 import (
 	"fmt"
+	"github.com/eaciit/dbox"
+	_ "github.com/eaciit/dbox/dbc/mongo"
 	"net/http"
-	"time"
 )
+
+var conn dbox.IConnection
+
+type Counter struct {
+	Count int
+}
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, fmt.Sprintf("<h1>You access this page at %s</h1>", time.Now().Format("15:04:05")))
+		csr, err := conn.NewQuery().From("counter").Cursor(nil)
+		if err != nil {
+			fmt.Println("error create cursor", err.Error())
+			return
+		}
+		counter := Counter{}
+		err = csr.Fetch(&counter, 1, false)
+		if err != nil {
+			fmt.Println("error fetch data", err.Error())
+			return
+		}
+		counter.Count++
+		fmt.Fprint(w, fmt.Sprintf("<h1>Hello Container World! I have been seen %d times</h1>", counter.Count))
+		err = conn.NewQuery().From("counter").Save().Exec(map[string]interface{}{
+			"data": Counter{counter.Count},
+		})
+		if err != nil {
+			fmt.Println("error save data", err.Error())
+			return
+		}
 	}
 }
 
+func prepareConnection() {
+	ci := &dbox.ConnectionInfo{"localhost:27017", "local", "", "", nil}
+	var err error
+	conn, err = dbox.NewConnection("mongo", ci)
+	if err != nil {
+		return
+	}
+	err = conn.Connect()
+	if err != nil {
+		return
+	}
+	return
+}
+
 func main() {
+	prepareConnection()
 	http.HandleFunc("/", handlerFunc)
 	port := "8003"
 	fmt.Println("this server run at port", port)
